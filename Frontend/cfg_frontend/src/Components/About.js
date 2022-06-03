c2e81f75cd1bc016149a7e8fc5264a3b4428e1e9
Aditi <aditisrivastava2609@gmail.com>
2022-06-03 20:36:16 +0530

    Added components folder

---
+++ Frontend/cfg_frontend/src/Components/About.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section id="about" className="about">
+      <div className="container" data-aos="fade-up">
+        <div className="section-header">
+          <h2>About Us</h2>
+          <p>Coding Octets</p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
